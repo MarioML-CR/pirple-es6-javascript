ed2d8d0c0b2944995aeeba916297aa5240803599
Mario Martínez L <mmartinezl@ucenfotec.ac.cr>
2021-09-30 09:26:02 -0600

    se agrega el módulo build tools

---
+++ js/build-tools.js
@@ -0,0 +1,12 @@
+/*
+Instalar en pc
+Si bien puede instalar Babel CLI globalmente en su máquina, es mucho mejor instalarlo localmente proyecto por proyecto.
+
+Hay dos razones principales para esto.
+
+Diferentes proyectos en la misma máquina pueden depender de diferentes versiones de Babel, lo que le permite actualizarlos individualmente.
+No tener una dependencia implícita del entorno en el que está trabajando hace que su proyecto sea mucho más portátil y más fácil de configurar.
+Podemos instalar Babel CLI localmente ejecutando:
+
+npm install --save-dev @babel/core @babel/cli
+*/
